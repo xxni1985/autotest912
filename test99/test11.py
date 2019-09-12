5bafd6b48780291c50dae98e9723078a5f7c5c5e
xxni1985 <952001015@qq.com>
2019-09-12 14:02:20 +0800

    初始化
    comit-by:952001015@qq.com

---
+++ test99/test11.py
@@ -0,0 +1,21 @@
+#coding:utf8
+'''
+Created on 2019��9��11��
+
+@author: admin
+'''
+from selenium import webdriver
+from asyncio.tasks import sleep
+
+exepath="../lib/chromedriver.exe"
+class Web():
+    def __init__(self):
+        self.driver=webdriver.Chrome(executable_path=exepath)
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(10)
+    
+    def close(self):
+        sleep(4)
+        self.driver.quit()
+        
+        
---
+++ test99/testpy.py
@@ -0,0 +1,14 @@
+#coding=utf8
+'''
+Created on 2019��9��12��
+
+@author: admin
+'''
+
+from test99.test11 import Web
+
+web=Web()
+web.driver.get("https://www.baidu.com")
+
+web.close()
+
